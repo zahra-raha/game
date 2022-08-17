a2e975540af0d9641cc1b517832f08ed705e6515
Zahra Hassani <raha.zahra1375@gmail.com>
2022-08-17 12:58:51 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+print("Lets have some fan today!")
+player_name = input("Please enter your name: \n")
+
+print(f"Hello {player_name}. Welcome play zoon!")
+print("Lets select with game you wanna play today")
+print("Enter 1 for  Rock, Paper, Scissors!")
+game = input()
+print(f"You selected {game}. Good Luck!")
